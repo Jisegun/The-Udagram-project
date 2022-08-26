830a0adc9834f3850e0ba6f806d777acca5d50cf
Jisegun <safesty1@gmail.com>
2022-08-26 11:12:38 +0200

    This is the code fron jisegun as of project 2

---
+++ README_project.md
@@ -0,0 +1,7 @@
+This file contains both endpoints for completed and accepted valid request, and URL for running EB deployement on Postman
+
+This is an  endpoint in src/server.ts is completed and accepts valid requests:
+http://localhost:8082/filteredimage?image_url=https://disruptops.com/wp-content/uploads/2018/10/how-s3-buckets-become-public-631b-800x505.jpg
+
+This is an endpoint URL for a running elastic beanstalk deployment (EB_URL),
+http://udagram-ajisafe-dev-dev.us-east-1.elasticbeanstalk.com/filteredimage?image_url=https://disruptops.com/wp-content/uploads/2018/10/how-s3-buckets-become-public-631b-800x505.jpg
---
+++ src/server.ts
@@ -0,0 +1,59 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import {filterImageFromURL, deleteLocalFiles} from './util/util';
+
+(async () => {
+
+  // Init the Express application
+  const app = express();
+
+  // Set the network port
+  const port = process.env.PORT || 8082;
+  
+  // Use the body parser middleware for post requests
+  app.use(bodyParser.json());
+
+
+  /**************************************************************************** */
+       //  GET /filteredimage?image_url={{URL}}
+  app.get("/filteredimage", async (req, res)=>{
+
+       // 1. validate the image_url query
+
+    const image_url = req.query.image_url.toString();
+  try {
+    if(!image_url)
+      return res.status(400).send(`Inavlid Image url!`);
+    
+      // 2. call filterImageFromURL(image_url) to filter the image
+      const filterImage = await filterImageFromURL(image_url);
+
+     // 3. send the resulting file in the response
+        return res.status(200).sendFile(filterImage);
+
+    //  4. deletes any files on the server on finish of the response
+
+        res.on("finish", () => deleteLocalFiles([filterImage]));
+
+    }  
+    catch (error) {
+        return res.status(422).send("Image process error");
+      }
+        
+  });
+
+  
+  
+  // Root Endpoint
+  // Displays a simple message to the user
+  app.get( "/", async ( req, res ) => {
+    res.send("try GET /filteredimage?image_url={{}}")
+  } );
+  
+
+  // Start the Server
+  app.listen( port, () => {
+      console.log( `server running http://localhost:${ port }` );
+      console.log( `press CTRL+C to stop server` );
+  } );
+})();
